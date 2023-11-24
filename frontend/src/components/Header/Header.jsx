d1cf7238189a9ae379bd244907b6f9b141d77f30
Luciano Cupertino <luciano.cupertino@hotmail.com>
2023-11-24 00:24:13 -0300

    feat:create-date-header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,66 @@
-import React from "react";
-import { HeaderBar } from "./Header.style.js";
+import React, { useEffect, useState } from "react";
+import { CiCalendarDate } from "react-icons/ci";
+import * as S from "./Header.style.js";
 
 const Header = () => {
-  return <HeaderBar>Header</HeaderBar>;
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+
+    // Limpar o intervalo ao desmontar o componente
+    return () => clearInterval(intervalId);
+  }, []);
+
+  // Função para obter o nome do dia da semana
+  const getDayOfWeek = () => {
+    const daysOfWeek = [
+      "Domingo",
+      "Segunda-feira",
+      "Terça-feira",
+      "Quarta-feira",
+      "Quinta-feira",
+      "Sexta-feira",
+      "Sábado",
+    ];
+    return daysOfWeek[currentDate.getDay()];
+  };
+
+  const getMonthName = () => {
+    const months = [
+      "Janeiro",
+      "Fevereiro",
+      "Março",
+      "Abril",
+      "Maio",
+      "Junho",
+      "Julho",
+      "Agosto",
+      "Setembro",
+      "Outubro",
+      "Novembro",
+      "Dezembro",
+    ];
+    return months[currentDate.getMonth()];
+  };
+
+  const getCurrentTime = () => {
+    const hours = currentDate.getHours().toString().padStart(2, "0");
+    const minutes = currentDate.getMinutes().toString().padStart(2, "0");
+
+    return `${hours}:${minutes}`;
+  };
+
+  return (
+    <S.HeaderBar>
+      <S.DateContainer>
+        <CiCalendarDate />
+        {`${getDayOfWeek()}, ${currentDate.getDate()} de ${getMonthName()} de ${currentDate.getFullYear()}  -  ${getCurrentTime()}`}
+      </S.DateContainer>
+    </S.HeaderBar>
+  );
 };
 
 export default Header;
--- frontend/src/components/Header/Header.style.js
+++ frontend/src/components/Header/Header.style.js
@@ -4,5 +4,13 @@ export const HeaderBar = styled.div`
   display: flex;
   height: 70px;
   width: calc(100vw - 260px);
+  padding: 30px;
   background-color: #fff;
+`;
+
+export const DateContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  font-size: 1.2rem;
 `;
