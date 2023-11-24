dd59ff0f4434147818aa00f03dc52466f7ad07a9
Luciano Cupertino <luciano.cupertino@hotmail.com>
2023-11-23 22:00:36 -0300

    feat:reorder-repository

--- frontend/src/pages/Clients/Clients.jsx
+++ frontend/src/pages/Clients/Clients.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Clients = () => {
-  return <NavLink to="/clients/addclient">Inserir</NavLink>;
+  return <NavLink to="/clients/addclient">merda</NavLink>;
 };
 
 export default Clients;
